3c2ebdcd57fff32de288fd727b452603ca2d7c9f
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-11-12 17:15:00 -0600

    Now displays all results after a search (Title + release date)

--- apiTest.js
+++ apiTest.js
@@ -1,27 +1,33 @@
-const body = document.getElementsByTagName("body");
-let gameName = document.createElement("h1");
-let gameDate = document.createElement("p");
+const body = document.getElementById("results");
 
-var searchResults = () => fetch("https://api.rawg.io/api/games?key=bd8ac961089f4fb694db12c2ad50dfcb&search=harry-potter")
-  .then(function (response) {
-    return response.json();
-
-  })
-  .then(function (data){
-    console.log(data);
-    var results = data.results;
-    results.forEach((game) => {
-      // console.log(game);
-      var resultsArray = [];
-      resultsArray.push({
+var searchResults = async () => fetch("https://api.rawg.io/api/games?key=bd8ac961089f4fb694db12c2ad50dfcb&search=harry-potter")
+.then(function (response) {
+  return response.json();
+  
+})
+.then(await function (data){
+  console.log(data);
+  var results = data.results;
+  results.forEach((game) => {
+    // console.log(game);
+    var resultsArray = [];
+    // var platforms = [];
+    // game.platforms.forEach((platform) => {
+    //   platforms.push(platform.name);
+    //   console.log(platform.slug)
+    // });
+    resultsArray.push({
       "title": game.name,
-      "released": game.released
+      "released": game.released,
+      // "platforms": platforms
     });
     console.log(resultsArray)
-    // gameName.appendChild(game.name);
-    // gameDate.appendChild(game.released);
-    // body.appendChild(gameName);
-    // body.appendChild(gameDate);
+    const gameName = document.createElement("h1");
+    const gameDate = document.createElement("p");
+    gameName.textContent = game.name;
+    gameDate.textContent = game.released;
+    body.appendChild(gameName);
+    body.appendChild(gameDate);
   }
     )
   })
