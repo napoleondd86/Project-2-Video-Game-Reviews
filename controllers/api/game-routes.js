6c56084f11331f7382eb6dff5e35a5805c0d086f
KWensmann <kurtwensmann@gmail.com>
2023-11-15 16:48:59 -0600

    feedback finished

--- controllers/api/game-routes.js
+++ controllers/api/game-routes.js
@@ -55,6 +55,15 @@ router.get('/:id', async (req, res) => {
 // create a new Game
 router.post('/', async (req, res) => {
   try{
+    const allGamesData = await Game.findOne({
+      where: {
+        id: req.body.id,
+      }
+    })
+    if(allGamesData){
+      res.status(400).end();
+      return;
+    }
     const gameData = await Game.create(req.body);
     res.status(200).json(gameData);
   } catch (err) {
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 const search = document.getElementById("searchBar");
 const searchTerm = document.getElementById("query");
 const playedSubmit = document.getElementById("playedSubmit")
+const optionsForm = document.getElementById("review-options-form")
 // const logo = document.querySelector("")
 
 const searchFunction = () => {
@@ -27,43 +28,63 @@ search.addEventListener("submit", (event) => {
 });
 
 
-// FORM FROM FEEDBACK 
+// FORM PLAY FEEDBACK 
 const playedData = document.querySelector(".played-form");
-const playedCheckbox = playedData.querySelector("#played");
-const wantToPlayCheckbox = playedData.querySelector("#want-play");
+const playedCheckbox = document.querySelector("#played");
+const wantToPlayCheckbox = document.querySelector("#want-play");
 
 const playedFeedback = async (event) => {
   event.preventDefault();
-
+  const played = playedCheckbox.checked;
   const gameId = window.location.href.split('game/')[1].split("?")[0]
+  let feedbackData = {};
+
+  if (played) {
+    const rating = document.querySelector("#quantity").value;
+    const hoursArray = Array.from(document.querySelectorAll("[name='hours']"));
+    const hours = hoursArray.find(hour => hour.checked).value;
+    const ageArr = Array.from(document.querySelectorAll("[name='age']"));
+    const age = ageArr.find(age => age.checked).value;
+    const review = document.querySelector("[name='review']").value;
+    feedbackData = { rating, hours, age, review }
+  }
+
   await fetch('/api/games', {
     method: "POST",
     body: JSON.stringify({id: gameId}),
     headers: { 'Content-Type': 'application/json' },
   })
+  
  
-  console.log(gameId)
-  const played = playedCheckbox.checked;
-  const wantToPlay = wantToPlayCheckbox.checked;
-  if (!played && !wantToPlay) return
+  const want_play = wantToPlayCheckbox.checked;
+
+ 
+  if (!played && !want_play) return
   const response = await fetch('/api/feedbacks/played', {
     method: 'POST',
-    body: JSON.stringify({played, game_id: gameId}),
+    body: JSON.stringify({played, game_id: gameId, ...feedbackData}),
     headers: { 'Content-Type': 'application/json' },
   });
   const result = await response.json();
   console.log(result);
+};
 
-
+const togglePlayed = (event) => {
+  if (playedCheckbox.checked) {
+    optionsForm.setAttribute("style", "display: block")
+  } else {
+    optionsForm.setAttribute("style", "display: none")
+  }
+}
+// user_id: userId
   // if (response.ok) {
   //   alert("Thanks for your feedback!")
   //   document.location.replace('/game/');
   // } else {
   //   alert(result);//// RESULT.PAYLOAD -- ALSO WOULDNT BE AN ALERT
   // }
-};
 playedSubmit.addEventListener("click", playedFeedback);
-
+playedData.addEventListener("change", togglePlayed)
 // Rating Initialization
 // $(document).ready(function() {
 //   $('#rateMe3').mdbRate();
